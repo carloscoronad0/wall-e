a145ab00d12e0b0891687886ca20b5bfba5cf803
Jud4 <Jud4@users.noreply.github.com>
2022-10-27 09:29:53 -0400

    initial controller

--- controllers/wall-e-controller/wall-e-controller.py
+++ controllers/wall-e-controller/wall-e-controller.py
@@ -4,29 +4,32 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
-robot = Robot()
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+if __name__== "__main__":
+    # create the Robot instance.
+    robot = Robot()
+    
+    # get the time step of the current world.
+    timestep = int(robot.getBasicTimeStep())
+    
+    
+    # You should insert a getDevice-like function in order to get the
+    # instance of a device of the robot. Something like:
+    #  motor = robot.getDevice('motorname')
+    #  ds = robot.getDevice('dsname')
+    #  ds.enable(timestep)
+    
+    # Main loop:
+    # - perform simulation steps until Webots is stopping the controller
+    while robot.step(timestep) != -1:
+        # Read the sensors:
+        # Enter here functions to read sensor data, like:
+        #  val = ds.getValue()
+    
+        # Process sensor data here.
+    
+        # Enter here functions to send actuator commands, like:
+        #  motor.setPosition(10.0)
+        pass
+    
+    # Enter here exit cleanup code.
+    
